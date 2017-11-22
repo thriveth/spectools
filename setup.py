45ed4413a15e398a314ff1d6b0a19aecaf4d82ed
Thøger Rivera-Thorsen <thoger.emil@gmail.com>
2017-11-22 18:28:11 +0100

    Pip/compatible package, yay

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='spectools',
+    version='0.1',
+    description='Helpers for 1D astrophysical spectroscopy',
+    url='https://github.com/thriveth/spectools/',
+    author='T. E. Rivera-Thorsen',
+    author_email='eriveth@uio.no',
+    packages=['spectools'],
+    license='GPL3'
+)

07cfbad3fc3e7a2a2c081e3918a5c4c82edd21ef
Thøger Rivera-Thorsen <thoger.emil@gmail.com>
2018-02-27 17:22:50 -0600

    fixed numpy references

--- spectools/bpt_tools.py
+++ spectools/bpt_tools.py
@@ -45,24 +45,24 @@ def draw_O123_diags(ax):
 
 def draw_N2_O3_diags(ax):
     ax.plot(
-        linspace(-4, 0, 200),
-        0.61 / (linspace(-4, 0, 200)-0.05) + 1.3,
+        np.linspace(-4, 0, 200),
+        0.61 / (np.linspace(-4, 0, 200)-0.05) + 1.3,
         'k--', zorder=1, label='Ka03'
     )  # Ka03
     ax.plot(
-        linspace(-4, 0.2, 200),
-        0.61 / (linspace(-4, 0.2, 200)-0.47) + 1.19,
+        np.linspace(-4, 0.2, 200),
+        0.61 / (np.linspace(-4, 0.2, 200)-0.47) + 1.19,
         'k-', zorder=1, label='Ke01'
     )  # Ke01
-    return(ax)
+    return ax
 
 def draw_O13_diags(ax):
     ax.plot(
-        linspace(-1.13, -.5, 200),
-        1.18 * (linspace(-1.13, -.5, 200)-0.0) + 1.3,
+        np.linspace(-1.13, -.5, 200),
+        1.18 * (np.linspace(-1.13, -.5, 200)-0.0) + 1.3,
         'k-', zorder=1, label='Ke06')  # Ke06
     ax.plot(
-        linspace(-4, -0.89, 200),
-        0.73 / (linspace(-4, -0.89, 200) + 0.59) + 1.33,
+        np.linspace(-4, -0.89, 200),
+        0.73 / (np.linspace(-4, -0.89, 200) + 0.59) + 1.33,
         'k-', zorder=1) # Ke06
     return ax

9f7c95379ed8b8a63d986aaba262cbb884267151
Thøger Rivera-Thorsen <thoger.emil@gmail.com>
2018-03-27 15:58:04 +0900

    Improved kinematics handling
    including  adding an fwhm mode

--- spectools/dynvel.py
+++ spectools/dynvel.py
@@ -8,9 +8,14 @@ from astropy.table import Table
 from astropy.io import ascii
 import astropy.units as u
 from spectools.spectools import SimpleMaskGUI
+from statsmodels.nonparametric.kernel_regression import KernelReg
+from tqdm import tqdm
+
 
 class DynVelGUI(SimpleMaskGUI):
-    """ Computes dynamic velocity properties of an absorption line"""
+    """ Computes dynamic velocity properties of an absorption line
+    NB! Not implemented yet, this is just an empty skeleton.
+    """
     plot_added = None
     iterations = 100
     def _onselect(self, event):
@@ -21,7 +26,24 @@ class DynVelGUI(SimpleMaskGUI):
         pass
 
 
-def dynvel(wave, data, errs, iterations=100, kind='absorb'):
+def FWHM(wave, pertdata, mode='data'):
+    """ Mode can be data, ll, lc
+    """
+    fwhms = []
+    for i in tqdm(range(pertdata.shape[0])):
+        data = pertdata[i, :]
+        if mode in ['ll', 'lc']:
+            lle = KernelReg(data, wave, 'c', reg_type=mode)
+            data = lle.fit()[0]
+        iplwave = np.linspace(wave.min(), wave.max(), 1000)
+        ipldata = np.interp(iplwave, wave, data)
+        iplidx = np.where(ipldata > ipldata.max()/2)[0]
+        vmin, vmax = iplidx.min(), iplidx.max()
+        fwhms.append(iplwave[vmax] - iplwave[vmin])
+    return np.array(fwhms)
+
+
+def dynvel(wave, data, errs, iterations=100, kind='absorb', fwhm_mode='ll'):
     """Computes a range of kinematic properties from a spectral line.
     The line is assumed continuum normalized.
     EW is given in km/s, as the function only knows velocity and flux.
@@ -51,6 +73,8 @@ def dynvel(wave, data, errs, iterations=100, kind='absorb'):
     five = np.absolute(accu - 0.05).argmin(axis=1)
     fifty = np.absolute(accu - 0.5).argmin(axis=1)
     ninetyfive = np.absolute(accu - 0.95).argmin(axis=1)
+    # lledata =
+    fwhm = FWHM(wave, pertdata, mode=fwhm_mode)
     mins = pertdata.argmax(axis=1)
     imins = (1 - pertdata).min(axis=1)
     fivevels = np.array(
@@ -107,6 +131,13 @@ def dynvel(wave, data, errs, iterations=100, kind='absorb'):
         'mean': imins[1:].mean(),
         'percentiles': np.percentile(imins[1:], [2.5, 16, 50, 84, 97.5]),
     }
+    fwhmout = {
+        'ml': fwhm[0],
+        'mean': fwhm.mean(),
+        'percentiles': np.percentile(fwhm[1:], [2.5, 16, 50, 84, 97.5]),
+        'stddev': fwhm[1:].std(),
+        'mode': fwhm_mode,
+    }
     outdict = {
         # 'EW': EW,
         'iterations':iterations,
@@ -120,18 +151,24 @@ def dynvel(wave, data, errs, iterations=100, kind='absorb'):
         'perturbed': pertdata,
         'cumsum': accu,
         'Velocity': wave,
+        'fwhm': fwhmout,
     }
     return outdict
 
 
-def dynvelplot(indict, ax=None, plotiters=True, color1='black', color2='C0'):
+def dynvelplot(indict, ax=None, plotiters=True, color1='gray', color2='C0', ):
     """Takes as input the output dict from the dynvel() function"""
     if not ax:
         fig, ax = plt.subplots(1)
     ax2 = plt.twinx(ax=ax)
     vels = indict['Velocity']
     if plotiters:
-        iterations = max(indict['iterations'], 500)
+        iterations = min(indict['iterations'], 500)
+        # if indict['iterations'] > 500:
+        #     iterations = 500
+        # else:
+        #     iterations = indict['iterations']
+
         for i in np.arange(iterations):
             ax2.plot(vels, indict['cumsum'][i, :], color=color1, ls='-', lw=0.1, alpha=.2)
             #ax.plot(vels, 1-indict['perturbed'][i, :], color=color2, ls='-', lw=0.1, alpha=.2)
@@ -150,22 +187,22 @@ def dynvelplot(indict, ax=None, plotiters=True, color1='black', color2='C0'):
     ax.axhline(1, color='k', ls=':')
     ax.axhline(0, color='k', ls='--')
     ax.axvline(indict['v5pct']['ml'], color='darkgray', ls='--')
-    #ax.axvspan(
-    #    indict['v5pct']['ml']-indict['v5pct']['stddev'],
-    #    indict['v5pct']['ml'] + indict['v5pct']['stddev'],
-    #    facecolor='lightgray', edgecolor='darkgray', alpha=.5, zorder=0,
-    #)
+    # ax.axvspan(
+    #     indict['v5pct']['ml']-indict['v5pct']['stddev'],
+    #     indict['v5pct']['ml'] + indict['v5pct']['stddev'],
+    #     facecolor='lightgray', edgecolor='darkgray', alpha=.5, zorder=0,
+    # )
     ax.axvline(
         indict['v95']['ml'],
         indict['v95']['mean'],
         color='darkgray', ls='--'
     )
-    #ax.axvspan(
-    #    indict['v95']['ml']-indict['v95']['stddev'],
-    #    indict['v95']['ml'] + indict['v95']['stddev'],
-    #    facecolor='lightgray', edgecolor='darkgray',
-    #    alpha=.5, zorder=0,
-    #)
+    # ax.axvspan(
+    #     indict['v95']['ml']-indict['v95']['stddev'],
+    #     indict['v95']['ml'] + indict['v95']['stddev'],
+    #     facecolor='lightgray', edgecolor='darkgray',
+    #     alpha=.5, zorder=0,
+    # )
     ax.axvline(
         indict['vint']['ml'],
         indict['vint']['mean'],
@@ -203,10 +240,52 @@ def dynvelplot(indict, ax=None, plotiters=True, color1='black', color2='C0'):
     return fig, ax, ax2
 
 
-def pprint_dynvel(indict):
-    s1 = ''.join([r"$v_{5\%} =", "{:.0f} \pm {:.0f}$ km/s".format(indict['v5pct']['ml'], indict['v5pct']['stddev']),])
-    s2 = ''.join([r"$v_{95\%} =", "{:.0f} \pm {:.0f}$ km/s".format(indict['v95']['ml'], indict['v95']['stddev']),])
-    s3 = ''.join([r"$v_{\mathrm{int}} =", "{:.0f} \pm {:.0f}$ km/s".format(indict['vint']['ml'], indict['vint']['stddev']),])
-    s4 = ''.join([r"$v_{min} =", "{:.0f} \pm {:.0f}$ km/s".format(indict['vmin']['ml'], indict['vmin']['stddev']),])
-    return "\n".join([s1, s3, s4, s2])
+def pm_string(percentiles):
+    """ Assumes percentiles are 2.5th, 16, 50, 84, 97.5th
+    """
+    val, upper, lower = percentiles[2], percentiles[3], percentiles[1]
+    plusval = upper - val
+    minusval = val - lower
+    S = "".join(
+        [
+            "${:.0f}".format(val),
+            r"^{+",
+            "{:.0f}".format(plusval),
+            r"}_{-",
+            "{:.0f}".format(minusval),
+            r"}$ ",
+        ])
+    return S
+
+def pprint_dynvel(indict, errors='std'):
+    """ Errors can be 'std' or 'pm'
+    """
+    if errors == 'std':
+        s1 = ''.join( [r"$v_{5\%} =", "{:.0f} \pm {:.0f}$ km/s"
+                       .format(indict['v5pct']['ml'], indict['v5pct']['stddev']),])
+        s2 = ''.join([r"$v_{95\%} =", "{:.0f} \pm {:.0f}$ km/s"
+                      .format(indict['v95']['ml'], indict['v95']['stddev']),])
+        s3 = ''.join([r"$v_{\mathrm{int}} =", "{:.0f} \pm {:.0f}$ km/s"
+                      .format(indict['vint']['ml'], indict['vint']['stddev']),])
+        s4 = ''.join([r"$v_{min} =", "{:.0f} \pm {:.0f}$ km/s"
+                      .format(indict['vmin']['ml'], indict['vmin']['stddev']),])
+        s5 = ''.join(["FWHM = {:.0f} $\pm$ {:.0f}"
+                      .format(indict['fwhm']['ml'], indict['fwhm']['stddev']),])
+    elif errors == 'pm':
+        s1 = ''.join([r"$v_{5\%} =$",
+                      pm_string(indict['v5pct']['percentiles']),
+                      "km/s"])
+        s2 = ''.join([r"$v_{95\%} =$",
+                      pm_string(indict['v95']['percentiles']),
+                      "km/s"])
+        s3 = ''.join([r"$v_{\mathrm{int}} =$",
+                      pm_string(indict['vint']['percentiles']),
+                      "km/s"])
+        s4 = ''.join([r"$v_{min} =$",
+                      pm_string(indict['vmin']['percentiles']),
+                      "km/s"])
+        s5 = ''.join([r"FWHM $=$",
+                      pm_string(indict['fwhm']['percentiles']),
+                      "km/s"])
+    return "\n".join([s1, s3, s4, s2, s5])
 
